7a2edd1831a0a83070b6257c58fb9e02cb751ff2
goat-py <donvonwiley098@gmail.com>
2017-07-05 19:11:10 -0400

    Had it set to levelnames instead of levelname kek

--- goatbot.py
+++ goatbot.py
@@ -5,7 +5,7 @@ import os
 import requests
 
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(name)s - %(levelnames)s - %(message)s')
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
 
