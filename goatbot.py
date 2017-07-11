e387ce913ee45b5a6f3a9c0a5ae5137dfd8d10a4
goat-py <donvonwiley098@gmail.com>
2017-07-11 03:26:54 -0400

    Changed logger level from DEBUG to INFO

--- goatbot.py
+++ goatbot.py
@@ -5,7 +5,7 @@ import logging
 import os
 import requests
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
